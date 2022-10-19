9a886409bf6dd6d5293860d00010c1b167d4e88c
perchik_air <1919ars@gmail.com>
2022-10-19 19:11:52 +0300

    more

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ int main() {
 - [youtube | C++ Crash Course For Beginners
   ](https://www.youtube.com/watch?v=1v_4dL8l8pQ&t=3464s&ab_channel=TraversyMedia)
 
-- []()
+- [w3schools | C++ Tutorial](https://www.w3schools.com/cpp/)
 - []()
 - []()
 - []()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
 int main() {
-    std::cout << "abc";
+    int a = 10;
+    int b = 3;
+    int prev_a = a;
+    a = b;
+    b = prev_a;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    int z = a % b;
+    std::cout << z << std::endl;
+    ++z;
+    std::cout << z << std::endl;
+    // This is comment
+    /*
+     *
+     *
+     *
+     * */
     return 0;
 };
